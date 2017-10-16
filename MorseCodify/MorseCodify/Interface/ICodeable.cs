fcbf36d094446e1cef1a25d9761ebcd982cff794
Bryan Blakeney <blakeneybk@gmail.com>
2017-10-16 11:59:42 -0700

    Moving to desktop

---
+++ MorseCodify/MorseCodify/Interface/ICodeable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MorseCodify.Interface
+{
+    interface ICodeable
+    {
+        string Encode(string input, Dictionary<char, string> codex);
+    }
+}
---
+++ MorseCodify/MorseCodify/Service/MorseCoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MorseCodify.Interface;
+
+namespace MorseCodify.Service
+{
+    class MorseCoder:ICodeable
+    {
+        public string Encode(string input, Dictionary<char, string> codex)
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (var letter in input)
+            {
+                s.Append(codex.ContainsKey(letter) ? codex[letter]+" " : String.Empty);
+            }
+            return s.ToString().Trim();
+        }
+    }
+}
